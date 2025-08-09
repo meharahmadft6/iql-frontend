45fee18b29543cab0b23a3785e8e5bca931e854e
meharahmadft6 <167222938+meharahmadft6@users.noreply.github.com>
2025-08-09 17:30:47 +0500

    add production url of ec2

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURL = `https://43.205.214.209/api/`;
+export const baseURL = "https://api.infinityquotientlearning.com";
 // export const baseURL = `http://localhost:5000/api`;
 
 export const API = axios.create({
