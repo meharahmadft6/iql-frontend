2dae6f9f1561dab0d4414ead6afa379c6e46301b
meharahmadft6 <167222938+meharahmadft6@users.noreply.github.com>
2025-08-09 12:56:20 +0500

    add production url consoles

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURL = `https://iql-env.eba-mbt8mk3z.ap-south-1.elasticbeanstalk.com`;
+export const baseURL = `http://iql-env.eba-mbt8mk3z.ap-south-1.elasticbeanstalk.com/api`;
 // export const baseURL = `http://localhost:5000/api`;
 
 export const API = axios.create({
