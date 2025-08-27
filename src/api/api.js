b628a307990b77acc9a9f0df58951d14dc7af9a8
meharahmadft6 <167222938+meharahmadft6@users.noreply.github.com>
2025-08-27 16:56:00 +0500

    base url changed

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-// export const baseURL = "https://api.infinityquotientlearning.com/api";
-export const baseURL = `http://localhost:5000/api`;
+export const baseURL = "https://api.infinityquotientlearning.com/api";
+// export const baseURL = `http://localhost:5000/api`;
 
 export const API = axios.create({
   baseURL: baseURL,
 });
-  
+
 API.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
